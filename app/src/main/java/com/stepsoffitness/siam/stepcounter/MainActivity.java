b6db0908738f7b3080c4675807d9b6e33206140e
Siam17 <nasiam007@gmail.com>
2018-04-27 04:41:06 +0600

    Initial commit

---
+++ app/src/main/java/com/stepsoffitness/siam/stepcounter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.stepsoffitness.siam.stepcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
